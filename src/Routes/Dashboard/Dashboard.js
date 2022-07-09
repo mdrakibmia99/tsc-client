fd98a925867207552cda1b63e456931dd6ae234a
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-09 16:32:48 +0600

    dashboard customlink path setup

--- src/Routes/Dashboard/Dashboard.js
+++ src/Routes/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
 
 const Dashboard = () => {
@@ -9,17 +10,17 @@ const Dashboard = () => {
   <div class="drawer-content flex flex-col items-center justify-center">
     {/* <!-- Page content here --> */}
     <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-  
+   <Outlet></Outlet>
   </div> 
   <div class="drawer-side">
     <label for="my-drawer-2" class="drawer-overlay"></label> 
     <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
       {/* <!-- Sidebar content here --> */}
-      <li> <CustomLink to=''>Add Student</CustomLink> </li>
-      <li> <CustomLink to=''>View Student</CustomLink> </li>
-      <li> <CustomLink to=''>Add teacher</CustomLink> </li>
-      <li> <CustomLink to=''>View Teacher</CustomLink> </li>
-      <li> <CustomLink to=''>Todo</CustomLink> </li>
+      <li> <CustomLink to='dashboard'>View Student</CustomLink> </li>
+      <li> <CustomLink to='/dashboard/addStudent'>Add Student</CustomLink> </li>
+      <li> <CustomLink to='/dashboard/addTeacher'>Add teacher</CustomLink> </li>
+      <li> <CustomLink to='/dashboard/viewTeacher'>View Teacher</CustomLink> </li>
+      <li> <CustomLink to='/dashboard/todo'>Todo</CustomLink> </li>
     </ul>
   
   </div>
